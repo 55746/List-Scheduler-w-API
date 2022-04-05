7d0e7978e4ed3d32b6af69940463392a31d2df88
55746 <Lawhead.gavin@live.jobcorps.org>
2022-04-05 19:14:05 +0000

    almost done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,107 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [todolist, setTodolist] = useState([]);
+	const [item, setItem] = useState("");
+
+	const addItem = (newItem) => {
+		let newList = [...todolist, { label: newItem, done: false }];
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/createdname", {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(newList),
+			redirct: "follow",
+		})
+			// .then((response) => response.json())
+			.then((response) => {
+				response.status === 200 ? setTodolist(newList) : "";
+			})
+			.catch((error) => console.log("error", error));
+	};
+	useEffect(() => {
+		let requestOptions = {
+			method: "GET",
+			redirect: "follow",
+		};
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/createdname",
+			requestOptions
+		)
+			.then((response) => response.json())
+			.then((result) => {
+				setTodolist(result);
+			})
+			.catch((error) => console.log("error", error));
+	}, []);
+
+	const deletes = (index) => {
+		const par = todolist.filter((list, i) => index !== i);
+		setTodolist(par);
+	};
+	console.log(todolist);
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div>
+				<h1>Data</h1>
+			</div>
+			<div className="input-group mb-3">
+				<div className="input-group-prepend">
+					<input
+						type="text"
+						className="form-control"
+						placeholder=""
+						onChange={(e) => setItem(e.target.value)}
+						value={item}
+					/>
+					<a
+						onClick={() => {
+							if (item === "")
+								return alert("you cant do that Jimbo");
+							// setTodolist([...todolist, item]);
+							// addItem(item);
+							// setItem("");
+							// console.log("updated");
+							addItem(item);
+						}}
+						type="btn btn-primary"
+						className="input-group-text"
+						id="basic-addon1">
+						Button
+					</a>
+				</div>
+			</div>
+			<ul>
+				{todolist &&
+					todolist.map((list, index) => {
+						return (
+							<li key={index} className="boxtype">
+								<div>{list.label}</div>
+								<div className="deletekey">
+									<a
+										className="deletekey btn "
+										onClick={() => {
+											deletes(index);
+										}}>
+										X
+									</a>
+								</div>
+							</li>
+						);
+					})}
+			</ul>
+			<span>{todolist.length + " items"}</span>
+			<br></br>
+		</>
 	);
 };
 
 export default Home;
+
+// let requestOptions = {
+// 	method: "PUT",
+// 	// headers: myHeaders,
+// 	redirect: "follow",
+// 	body: raw,
+// };
